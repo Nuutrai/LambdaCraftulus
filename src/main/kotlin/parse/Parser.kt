f06ea7729822c78ca02ddb020d93c399b28710a4
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-03-27 00:36:08 +0100

    add a general Parser

--- src/main/kotlin/parse/Parser.kt
+++ src/main/kotlin/parse/Parser.kt
@@ -49,3 +49,38 @@ fun parseWithParens(tokens: LinkedList<Token>) : Expression {
         else -> throw IllegalArgumentException("Unexpected Token! Expected Var or LParen, got: $current")
     }
 }
+
+fun parse(tokens: LinkedList<Token>) : Expression {
+    when (val current = tokens.pop()) {
+        is Token.Var -> return Expression.Var(current.name)
+        is Token.Lambda -> {
+            val variable = verifyToken<Token.Var>(tokens.pop())
+            verifyToken<Token.Dot>(tokens.pop())
+
+            return Expression.Lambda(Expression.Var(variable.name), parse(tokens))
+        }
+        is Token.LParen ->
+            when (tokens.peek()) {
+                is Token.Lambda -> {
+                    tokens.pop()
+                    val variable = verifyToken<Token.Var>(tokens.pop())
+                    verifyToken<Token.Dot>(tokens.pop())
+                    val expr = parse(tokens)
+                    val lambda = Expression.Lambda(Expression.Var(variable.name), expr)
+                    if (tokens.peek() is Token.RParen){
+                        tokens.pop()
+                        return lambda
+                    }
+                    val apply = Expression.Apply(lambda, parse(tokens))
+                    verifyToken<Token.RParen>(tokens.pop())
+                    return apply
+                }
+                else -> {
+                    val expr = Expression.Apply(parse(tokens), parse(tokens))
+                    verifyToken<Token.RParen>(tokens.pop())
+                    return expr
+                }
+            }
+        else -> throw IllegalArgumentException("Unexpected Token! Expected Var, Lambda or LParen, got: $current")
+    }
+}
