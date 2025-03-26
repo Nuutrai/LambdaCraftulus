42710f7b44d327d41b74aa77844cb970539bad8a
mibers <woodrow.conran@gmail.com>
2025-03-26 19:05:26 +0000

    add proper logging for minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("net.minestom:minestom-snapshots:7589b3b655")
+    implementation("org.tinylog:slf4j-tinylog:2.7.0")
+    implementation("org.tinylog:tinylog-impl:2.7.0")
 }
 
 tasks.test {
