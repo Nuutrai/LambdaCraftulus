fc0038a789278f84cb1f62b4a5192b14b3ec865a
Lexî Nuutraî <info@nuutrai.com>
2025-03-27 03:26:33 +0100

    Fixed reduceAll by adding a reduceAt. Corrected replace to use variable names

--- src/main/kotlin/lambda/Reduction.kt
+++ src/main/kotlin/lambda/Reduction.kt
@@ -11,17 +11,28 @@ fun reduce(appl: Apply) : Expression {
 
 private fun replace(expr: Expression, replace: Var, with: Expression) : Expression {
     return when (expr) {
-        is Var -> if (expr == replace) with else expr
+        is Var -> if (expr.name == replace.name) with else expr
         is Lambda -> Lambda(expr.variable, replace(expr.body, replace, with))
         is Apply -> Apply(replace(expr.apply, replace, with), replace(expr.to, replace, with))
+    }
+}
+
+private fun reduceAt(expr: Expression, appl: Apply): Expression {
+    if (expr === appl)
+        return reduce(expr)
+
+    return when (expr) {
+        is Var -> expr
+        is Lambda -> Lambda(expr.variable, reduceAt(expr.body, appl))
+        is Apply -> Apply(reduceAt(expr.apply, appl), reduceAt(expr.to, appl))
     }
 }
 
 fun reduceAll(expr: Expression, strategy: (expr: Expression) -> Apply?) : Expression {
     var current = expr
     while (true) {
-        val reducible = strategy(expr) ?: break
-        current = reduce(reducible)
+        val reducible = strategy(current) ?: break
+        current = reduceAt(current, reducible)
     }
     return current
 }
